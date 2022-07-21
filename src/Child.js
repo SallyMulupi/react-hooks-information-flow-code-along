364f0a2243bf66573fea1d4b0d215e09faa0ce18
SallyMulupi <mulupisally2gmail.com>
2022-07-22 00:24:44 +0300

    add const newcolor

--- src/Child.js
+++ src/Child.js
@@ -4,4 +4,5 @@ import { getRandomColor } from "./randomColorGenerator.js";
 
 function Child({ onChangeColor, color }) {
   function handleClick() {
+    const newColor = getRandomColor();
     export default Child;
