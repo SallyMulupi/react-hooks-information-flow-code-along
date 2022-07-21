276af69ab5cf3cd8b40a520d5591e266049b3ef8
SallyMulupi <mulupisally2gmail.com>
2022-07-22 00:25:46 +0300

    add console log  for handleclick

--- src/Child.js
+++ src/Child.js
@@ -5,4 +5,7 @@ import { getRandomColor } from "./randomColorGenerator.js";
 function Child({ onChangeColor, color }) {
   function handleClick() {
     const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
+  console.log(handleClick)
     export default Child;
