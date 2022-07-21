10d85b8287f5f285d86323fd34d111b1c4a80f19
SallyMulupi <mulupisally2gmail.com>
2022-07-22 00:23:32 +0300

    add function child

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
-}
 
-export default Child;
+function Child({ onChangeColor, color }) {
+    export default Child;
